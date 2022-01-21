ea3c0a84215aac64d190463e52791eae1c6db06c
Joao Goncalves <joao@joaogoncalves.net>
2022-01-21 17:40:23 +0000

    atualizacao modelo de dados com id

--- js/index.js
+++ js/index.js
@@ -1,34 +1,44 @@
 // Modelo de dados, a ser representado na pagina (INPUTS)
 let livros = [
     {
-        title: 'Angular Com Typescript',
+        id: 0,
+        title: 'Angular com Typescript',
         author: "Yakov Fain",
         alreadyRead: true,
         imageUrl: 'angular.jpg',
+        imageUrlGr: 'angularGr.png',
     },
     {
+        id: 1,
         title: 'Blockchain com JS',
-        author: "Bina Ramamurthy",
+        author: "Someone Else",
         alreadyRead: false,
         imageUrl: 'blockchain.jpg',
+        imageUrlGr: 'blockchainGr.png',
     },
     {
-        title: 'Deep Learning com JS',
-        author: "Various Authors",
+        id: 2,
+        title: 'DeepLearning com JS',
+        author: "Someone Else",
         alreadyRead: true,
         imageUrl: 'deeplearning.jpg',
+        imageUrlGr: 'deeplearningGr.png',
     },
     {
-        title: 'Joy Of Javascript',
-        author: "Luis Ascencio",
-        alreadyRead: false,
-        imageUrl: 'joj.jpg',
-    },
-    {
-        title: 'React Hooks in Action',
-        author: "John Larsen",
+        id: 3,
+        title: 'Joy of Javascript',
+        author: "Someone Else",
         alreadyRead: true,
+        imageUrl: 'joj.jpg',
+        imageUrlGr: 'jojGr.png',
+    },
+    {
+        id: 4,
+        title: 'React Hooks',
+        author: "Someone Else",
+        alreadyRead: false,
         imageUrl: 'reacthooks.jpg',
+        imageUrlGr: 'reacthooksGr.png',
     },
 ];
 

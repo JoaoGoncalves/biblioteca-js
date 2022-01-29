419fa0d463eceb8225c9fb0d7638029421a031e3
Joao Goncalves <joao@joaogoncalves.net>
2022-01-29 09:04:36 +0000

    editar livro

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+
 document.addEventListener('DOMContentLoaded', init , false);
 
 function init(){
@@ -19,17 +20,26 @@ function init(){
     let imageUrl = document.getElementById('imgUrl');
     let imageUrlGr = document.getElementById('imgUrlGr');
 
+    //botoes de atualizar e acrwce tar livro
+    let submitBtn = document.getElementById('submitBtn');
+    let updateBtn = document.getElementById('updateBtn');
+
+    let livroAEditar;
+
+
+
 
     /// Bloco de eventos da applicação
-    filters.addEventListener('change', filterEvents, false);
-    filters.addEventListener('input', filterEvents, false);
+    filters.addEventListener('change', filterEvents, false);//seccao de filtros
+    filters.addEventListener('input', filterEvents, false);//seccao de filtros
 
-    grid.addEventListener('click', gridEvents, false);
-    popup.addEventListener('click', fecharPopup, false);
-
+    grid.addEventListener('click', gridEvents, false);//seccao da grid de articles de livros
     
-    formBook.addEventListener('submit', criarLivro, false);
+    popup.addEventListener('click', fecharPopup, false);// controle da popup
 
+    formBook.addEventListener('submit', criarLivro, false); // form de acrescentar livros
+
+    updateBtn.addEventListener('click', updateLivro, false);
 
 
 
@@ -57,10 +67,11 @@ function init(){
         livros.push(livro);
         mostrarLivros(livros);
         //console.log(livro);
+        
+        formBook.reset();
 
         e.preventDefault();
     }
-
 
     function filterEvents(e){
         //console.log(filterNotRead);
@@ -97,10 +108,68 @@ function init(){
             //console.log(imgGrSrc);
             mostrarPopup(imgGrSrc);
         }
+
+        if(e.target.className === 'editBtn'){
+            //console.log(e.target.dataset.id);
+            let id = e.target.dataset.id;
+            let livro = e.target.parentElement.parentElement;
+            livro.classList.add('atualizar');
+
+            atualizarLivro(id);
+        }
     }
 
 
     /// Bloco das funcionalidades da aplicação
+    function updateLivro (e){
+
+        //console.log(livroAEditar);
+
+        let livrosAtualizados = livros.map( livro => {
+            if(livro.id === livroAEditar.id){
+                return {
+                    ...livro,
+                    title: title.value,
+                    author: author.value,
+                    alreadyRead: alreadyRead.checked,
+                    imageUrl: imageUrl.value,
+                    imageUrlGr: imageUrlGr.value,
+                    xpto: 'ola mundo',
+                }
+            } else {
+                return livro;
+            }
+        });
+
+        livros = livrosAtualizados;
+        
+        mostrarLivros(livros);
+
+        formBook.reset();
+
+
+        e.preventDefault();
+    }
+
+
+    function atualizarLivro(id){
+
+        livroAEditar = livros.find( l => l.id == id);
+
+        //console.log(livroAEditar);
+
+        title.value = livroAEditar.title;
+        author.value = livroAEditar.author;
+        alreadyRead.checked = livroAEditar.alreadyRead;
+        imageUrl.value = livroAEditar.imageUrl;
+        imageUrlGr.value = livroAEditar.imageUrlGr;
+
+        submitBtn.className = 'hide';
+        updateBtn.className = 'show';
+
+    }
+
+
     function filtrarPorTitulo(texto){
         let livrosPesquisaTitulo = livros.filter( livro => livro.title.search(texto) > -1 );
 
@@ -153,7 +222,7 @@ function init(){
     
         arrayLivros.map( (livro) => {
             grid.innerHTML += `
-                <article>
+                <article >
                     <h1>${livro.title}</h1>
                     <h2>${livro.author}</h2>
                     <img  src='livros/${livro.imageUrl}' data-imggr='${livro.imageUrlGr}' />
